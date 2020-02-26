9cf6e41d6b2e1d9b38d41be13feb459052606c16
Kenan Yao <cauchy1992@gmail.com>
2020-02-26 23:12:48 +0800

    zhuomin's patch

--- planner/core/cbo_test.go
+++ planner/core/cbo_test.go
@@ -546,10 +546,17 @@ func (s *testAnalyzeSuite) TestInconsistentEstimation(c *C) {
 	// the `a = 5 and c = 5` will get 10, it is not consistent.
 	tk.MustQuery("explain select * from t use index(ab) where a = 5 and c = 5").
 		Check(testkit.Rows(
+<<<<<<< HEAD
 			"IndexLookUp_8 10.00 root ",
 			"├─IndexRangeScan_5(Build) 12.50 cop[tikv] table:t, index:a, b, range:[5,5], keep order:false",
 			"└─Selection_7(Probe) 10.00 cop[tikv] eq(test.t.c, 5)",
 			"  └─TableRowIDScan_6 12.50 cop[tikv] table:t, keep order:false",
+=======
+			"IndexLookUp_9 10.00 root ",
+			"├─IndexScan_6 12.50 cop[tikv] table:t, index:a, b, range:[5,5], keep order:false",
+			"└─Selection_8 10.00 cop[tikv] eq(test.t.c, 5)",
+			"  └─TableScan_7 12.50 cop[tikv] table:t, keep order:false",
+>>>>>>> 59c1fe2... planner, executor: support point get as a option of `DataSource`
 		))
 }
 
